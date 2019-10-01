1add612785994ef238c7a3e2047a5934046f6b17
PhyoLinMg <gangsterplm@gmail.com>
2019-10-01 12:23:35 +0630

    profile data added

--- app/src/main/java/com/elemental/atantat/ui/Activity/ProfileActivity.kt
+++ app/src/main/java/com/elemental/atantat/ui/Activity/ProfileActivity.kt
@@ -7,14 +7,21 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.elemental.atantat.R
 import com.elemental.atantat.viewmodel.ProfileViewModel.ProfileViewModel
 import com.elemental.atantat.viewmodel.ProfileViewModel.ProfileViewModelFactory
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.content_profile.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
-import org.kodein.di.android.x.kodein
+
 import org.kodein.di.generic.instance
+
+
+
+
 
 
 class ProfileActivity : AppCompatActivity(), KodeinAware{
@@ -22,11 +29,10 @@ class ProfileActivity : AppCompatActivity(), KodeinAware{
     override val kodein by kodein()
     private lateinit var profileViewModel:ProfileViewModel
     private val profileViewModelFactory:ProfileViewModelFactory by instance()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        val toolbar = findViewById<View>(com.elemental.atantat.R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
@@ -35,6 +41,12 @@ class ProfileActivity : AppCompatActivity(), KodeinAware{
         profileViewModel.loadUser()
 
         profileViewModel.getUser().observe(this, Observer {
+            profile_name.text=profileViewModel.getUser().value!!.name
+            header_name.text=profileViewModel.getUser().value!!.name
+            profile_email.text=profileViewModel.getUser().value!!.email
+            header_email.text=profileViewModel.getUser().value!!.email
+            profile_major.text=profileViewModel.getUser().value!!.major
+            profile_uni.text=profileViewModel.getUser().value!!.university
             Log.d("profile",profileViewModel.getUser().value.toString())
         })
 
